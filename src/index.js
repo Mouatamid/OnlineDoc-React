ca629abf13923e2dce3fa13be2fef92ab6fa2c32
Fahd Arhalai <fahd.arh@gmail.com>
2021-01-18 00:14:36 +0100

    configuration host et headers axios

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import './assets/js/script.js';
+import axios from 'axios';
+
+axios.defaults.baseURL = "http://localhost:8081";
+axios.defaults.headers.post['Content-Type'] = 'application/json';
+axios.defaults.headers.post['Accept'] = 'application/json';
 
 ReactDOM.render(
   <React.StrictMode>
