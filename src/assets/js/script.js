0d5b9802fac1eb5e005adc0d237603836558cecb
Fahd Arhalai <fahd.arh@gmail.com>
2021-01-18 00:16:38 +0100

    adaptation code jquery avec react components life cycle

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -20,30 +20,6 @@ import 'bootstrap/dist/js/bootstrap.min.js';
 		}
 	}
 	
-	// Sidebar
-	
-	if($(window).width() <= 991){
-	function init() {
-		$('.main-nav a').on('click', function(e) {
-			if($(this).parent().hasClass('has-submenu')) {
-				e.preventDefault();
-			}
-			if(!$(this).hasClass('submenu')) {
-				$('ul', $(this).parents('ul:first')).slideUp(350);
-				$('a', $(this).parents('ul:first')).removeClass('submenu');
-				$(this).next('ul').slideDown(350);
-				$(this).addClass('submenu');
-			} else if($(this).hasClass('submenu')) {
-				$(this).removeClass('submenu');
-				$(this).next('ul').slideUp(350);
-			}
-		});
-	}
-
-	// Sidebar Initiate
-	init();
-	}
-	
 	// Textarea Text Count
 	
 	var maxLength = 100;
@@ -74,14 +50,6 @@ import 'bootstrap/dist/js/bootstrap.min.js';
 				previous: 'fas fa-chevron-left'
 			}
 		});
-	}
-	
-	// Floating Label
-
-	if($('.floating').length > 0 ){
-		$('.floating').on('focus blur', function (e) {
-		$(this).parents('.form-focus').toggleClass('focused', (e.type === 'focus' || this.value.length > 0));
-		}).trigger('blur');
 	}
 	
 	// Mobile menu sidebar overlay
