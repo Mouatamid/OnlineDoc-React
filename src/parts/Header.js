f0f459c4c61f89d1d2d6938d91e93177f30c8e64
Fahd Arhalai <fahd.arh@gmail.com>
2021-01-18 00:19:18 +0100

    Header component

--- src/parts/Header.js
+++ src/parts/Header.js
@@ -1,22 +1,64 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
-import logo from '../assets/img/logo.png'
-import $ from "jquery";
+import React, { useEffect, useState } from 'react'
+import $ from 'jquery';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import logo from '../assets/img/logo.png';
+import AuthenticatedHeader from './AuthenticatedHeader';
+import UnauthenticatedHeader from './UnauthenticatedHeader';
 
-export default function Header() {
+export default function Header({authenticated, setAuthenticated}) {
+	const [auth, setAuth] = useState(false);
+	const [completed, setCompleted] = useState(false);
 	const [closeAction, setCloseAction] = useState(false);
+	const [subMenu, setSubMenu] = useState(null);
 
-	if(closeAction && document.getElementById("menu_close") != null){
-		document.getElementById("menu_close").click();
-		setCloseAction(false);
-	}
+	useEffect(() => {
+		if(closeAction && document.getElementById("menu_close") != null){
+			
+			document.getElementById("menu_close").click();
 
-	function subMenuClickHandler(e) {
+			if(subMenu != null && subMenu.hasClass('submenu')) {
+				subMenu.removeClass('submenu');
+				subMenu.next('ul').slideUp(350);
+			}
+
+			setCloseAction(false);
+		}
+	}, [closeAction, subMenu])
+
+    useEffect(() => {
+		setAuth(false);
+		setCompleted(false);
+        // send jwt to API to see if it's valid
+        const token = localStorage.getItem("token");
+        if (token) {
+            const url = "/api/login/authenticated/medecin";
+            const config = {
+                headers: {
+                    Authorization: 'Bearer ' + token
+                }
+            };
+            axios.post(url, null, config)
+                .then(response => {
+                    setAuth(true);
+                })
+                .catch(error => {
+                    setAuth(false);
+                    localStorage.removeItem("token");
+                })
+                .then(() => setCompleted(true));
+        } else {
+            setCompleted(true); // in case there is no token
+        }
+	}, [authenticated])
+	
+	const subMenuClickHandler = (e) => {
 		if($(window).width() <= 991){
 			if($(e.target).parent().hasClass('has-submenu')) {
 				e.nativeEvent.preventDefault();
 			}
 			if(!$(e.target).hasClass('submenu')) {
+				setSubMenu($(e.target));
 				$('ul', $(e.target).parents('ul:first')).slideUp(350);
 				$('a', $(e.target).parents('ul:first')).removeClass('submenu');
 				$(e.target).next('ul').slideDown(350);
@@ -27,56 +69,44 @@ export default function Header() {
 			}
 		}
 	}
-
-    return (
-        <header className="header">
-			<nav className="navbar navbar-expand-lg header-nav">
-				<div className="navbar-header">
-					<Link id="mobile_btn" to="#" onClick={() => setCloseAction(false)}>
-						<span className="bar-icon">
-							<span></span>
-							<span></span>
-							<span></span>
-						</span>
-					</Link>
-					<Link to="/" className="navbar-brand logo">
-						<img src={logo} className="img-fluid" alt="Logo" />
-					</Link>
-				</div>
-				<div className="main-menu-wrapper">
-					<div className="menu-header">
-						<Link to="/" className="menu-logo" onClick={() => setCloseAction(true)}>
+    
+    if (!completed){
+        return (
+			<header className="header">
+				<nav className="navbar navbar-expand-lg header-nav">
+					<div className="navbar-header">
+						<Link id="mobile_btn" to="#">
+							<span className="bar-icon">
+								<span></span>
+								<span></span>
+								<span></span>
+							</span>
+						</Link>
+						<Link to="/" className="navbar-brand logo">
 							<img src={logo} className="img-fluid" alt="Logo" />
 						</Link>
-						<Link id="menu_close" className="menu-close" to="#" onClick={() => setCloseAction(false)}>
-							<i className="fas fa-times"></i>
-						</Link>
 					</div>
-					<ul className="main-nav">
-						<li>
-							<Link to="/" onClick={() => setCloseAction(true)}>Home</Link>
-						</li>
-						<li>
-							<Link to="/blank" onClick={() => setCloseAction(true)}>Blank</Link>
-						</li>
-						<li className="has-submenu">
-							<Link to="#" onClick={(elm) => subMenuClickHandler(elm)}>Doctors <i className="fas fa-chevron-down"></i></Link>
-							<ul className="submenu">
-								<li><Link to="/doctor-dashboard" onClick={() => setCloseAction(true)}>Doctor Dashboard</Link></li>
-								<li><Link to="/appointments" onClick={() => setCloseAction(true)}>Appointments</Link></li>
-							</ul>
-						</li>
-						<li className="login-link">
-							<Link to="/login">Login / Signup</Link>
-						</li>
-					</ul>
-				</div>
-				<ul className="nav header-navbar-rht">
-					<li className="nav-item">
-						<Link className="nav-link header-login" to="/login">login | Signup </Link>
-					</li>
-				</ul>
-			</nav>
-		</header>
-    )
+					<div className="main-menu-wrapper">
+						<div className="menu-header">
+							<Link to="/" className="menu-logo">
+								<img src={logo} className="img-fluid" alt="Logo" />
+							</Link>
+							<Link id="menu_close" className="menu-close" to="#">
+								<i className="fas fa-times"></i>
+							</Link>
+						</div>
+					</div>
+				</nav>
+			</header>
+		)
+    }else{
+		return (auth === true ? 
+			<AuthenticatedHeader 
+				subMenuClickHandler = {(e) => subMenuClickHandler(e)}
+				setCloseAction = {(e) => setCloseAction(e)}
+				setAuthenticated = {(e) => setAuthenticated(e)} /> : 
+			<UnauthenticatedHeader 
+				subMenuClickHandler = {(e) => subMenuClickHandler(e)}
+				setCloseAction = {(e) => setCloseAction(e)} />);
+	}
 }
