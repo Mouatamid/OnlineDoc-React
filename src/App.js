7b167308e3e289eb3bf38129f8914813161f695e
Fahd Arhalai <fahd.arh@gmail.com>
2021-01-18 00:26:36 +0100

    adaptation selon le type d'utilisateur (authentifié ou pas)

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import $ from 'jquery';
 import './App.css';
 import Header from './parts/Header';
 import Home from './pages/Home';
 import Footer from './parts/Footer';
 import BlankPage from './pages/BlankPage';
-import { useState, useEffect } from 'react';
-import $ from 'jquery';
+import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
+import AuthenticatedRoute from './components/AuthenticatedRoute';
+import UnauthenticatedRoute from './components/UnauthenticatedRoute';
 
 function App() {
-  const [positioned, setPositioned] = useState(false);
+  const [authenticated, setAuthenticated] = useState(false);
 
-  function resizeInnerDiv() {
+  const resizeInnerDiv = () => {
     var height = $(window).height();	
     var header_height = $(".header").height();
     var footer_height = $(".footer").height();
@@ -18,25 +22,23 @@ function App() {
     var trueheight = setheight - footer_height;
     $(".page-container").css("min-height", trueheight);
   }
-	
-	useEffect(() => {
-      if($('.page-container').length > 0 ){
-          setPositioned(true);
+
+  useEffect(() => {
+      if($('.page-container').length > 0){
           resizeInnerDiv();
       }
-  
+
       $(window).resize(function(){
           if($('.page-container').length > 0 ){
-              setPositioned(true);
               resizeInnerDiv();
           }
       });
-  }, [positioned])
+  })
 
   return (
     <div className="main-wrapper">
       <Router>
-        <Header />
+        <Header authenticated = {authenticated} setAuthenticated = {(e) => setAuthenticated(e)} />
         <div className="page-container">
           <Switch>
             <Route exact path="/">
@@ -45,6 +47,12 @@ function App() {
             <Route path="/blank">
               <BlankPage />
             </Route>
+            <UnauthenticatedRoute path="/login">
+              <Login setAuthenticated = {(e) => setAuthenticated(e)} />
+            </UnauthenticatedRoute>
+            <AuthenticatedRoute path="/dashboard">
+              <Dashboard />
+            </AuthenticatedRoute>
           </Switch>
         </div>
         <Footer />
