8d1acf657f5a71c8a3ca7b1029d1e22af5ffcd6a
Fahd Arhalai <fahd.arh@gmail.com>
2021-02-13 22:58:09 +0100

    home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,21 @@
 import React from 'react'
+import { Link, Route } from 'react-router-dom';
 
 function Home() {
     return (
-        <div className="content">
+        <section className="section section-search">
             <div className="container-fluid">
-                <div className="row">
-                    <div className="col-12">
-                        <h5>Home</h5>
+                <div className="banner-wrapper">
+                    <div className="banner-header text-center">
+                        <h1>Manage Patients, Program your Schedule</h1>
+                        <hr />
+                        <p>Are you looking for top qualified Doctors ? Use our Android Application.</p>
+
+                        <Link to={{ pathname: "https://www.google.com" }} target="_blank"><img src={require("../assets/img/google-play-badge.png").default} width="200px" style={{marginTop: "20px"}} /></Link>
                     </div>
                 </div>
             </div>
-        </div>
+        </section>
     )
 }
 
