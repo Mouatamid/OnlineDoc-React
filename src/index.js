e9c6854e8b083762916eac7aa21851723b45bf58
Fahd Arhalai <fahd.arh@gmail.com>
2021-02-16 13:48:36 +0100

    ip address

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import './assets/js/script.js';
 import axios from 'axios';
 
-axios.defaults.baseURL = "http://34.69.186.11:8082";
+axios.defaults.baseURL = "http://35.193.152.73:8082";
 axios.defaults.headers['Content-Type'] = 'application/json; charset=utf-8';
 axios.defaults.headers['Accept'] = 'application/json; charset=utf-8';
 
